2d01bb5c339a288a3f144be073867130db568e47
Paradox Programming <nathaniel.fargo@gmail.com>
2019-05-01 08:36:22 -0600

    stuff

--- network.js
+++ network.js
@@ -5,9 +5,9 @@
 
 {
 
-function sigmoid(t) {
-	return 1/(1+Math.pow(Math.E, -t));
-};
+	function sigmoid(t) {
+		return 1/(1+Math.pow(Math.E, -t));
+	};
 
 }
 
@@ -17,55 +17,51 @@ function sigmoid(t) {
 
 {
 
-function Node(bias, index, outOf){
-	this.bias = bias;
-	this.index = index;
-	this.outOf = outOf;
-	this.val = 0;
-	this.inputs = [];
-	this.weights = [];
-};
-Node.prototype.set = function(val){
-	this.val = val;
-};
-Node.prototype.addInput = function(inNode){
-	var inputIndex = this.inputs.length;
-	this.inputs.push(inNode);
-	this.weights[inputIndex] = Math.random()*10-5;
-};
-Node.prototype.update = function(){
-	var val = 0;
-	for(var i = 0; i < this.inputs.length; i ++){
-		val += (this.inputs[i].val||0)*(this.weights[i]||0);
-	}
-	this.val = sigmoid(val);
-};
-Node.prototype.display = function(x,y,d){
-	fill(this.val*255);
-	ellipse(x,y,d,d);
-};
-Node.prototype.randomize = function(a){
-	function v(){
-		return Math.random() * a - a/2;
-	}
-	this.bias += v();
-	for(var i = 0; i < this.weights; i ++){
-		this.weights[i] += v();
-	}
+	function Node(bias, index, outOf){
+		this.bias = bias;
+		this.index = index;
+		this.outOf = outOf;
+		this.val = 0;
+		this.inputs = [];
+		this.weights = [];
+	};
+	Node.prototype.set = function(val){
+		this.val = val;
+	};
+	Node.prototype.addInput = function(inNode){
+		var inputIndex = this.inputs.length;
+		this.inputs.push(inNode);
+		this.weights[inputIndex] = this.weights[inputIndex] || Math.random() * 10 - 5;
+	};
+	Node.prototype.update = function(){
+		var val = 0;
+		for(var i = 0; i < this.inputs.length; i ++){
+			val += (this.inputs[i].val||0)*(this.weights[i]||0);
+		}
+		this.val = sigmoid(val);
+	};
+	Node.prototype.display = function(x,y,d){
+		fill(this.val*255);
+		ellipse(x,y,d,d);
+	};
+	Node.prototype.randomize = function(a){
+		function v(){
+			return Math.random() * a - a/2;
+		}
+		this.bias += v()/10;
+		for(var i = 0; i < this.weights; i ++){
+			this.weights[i] += v();
+		}
 
-};
-Node.prototype.copy = function(){
-	var newNode = {};
-	newNode.bias = this.bias;
-	newNode.index = this.index;
-	newNode.outOf = this.outOf;
-	newNode.val = 0;
-	newNode.inputs = [];
-	newNode.weights = [];
-	for(var i = 0; i < this.weights.length; i ++){
-		newNode.weights[i] = this.weights[i];
-	}
-};
+	};
+	Node.prototype.copy = function(){
+		var newNode = new Node(this.bias,this.index,this.outOf);
+		newNode.val = this.val;
+		for(var i = 0; i < this.weights.length; i ++){
+			newNode.weights[i] = this.weights[i];
+		}
+		return newNode;
+	};
 
 }
 
@@ -75,74 +71,78 @@ Node.prototype.copy = function(){
 
 {
 
-function Layer(nodes){
-	this.nodes = [];
-	for(var i = 0; i < nodes; i ++){
-		this.nodes[i] = new Node(Math.random()*10-5, i, nodes);
-	}
-	this.inputLayer = [];
-};
-Layer.prototype.set = function(arr){
-	for(var i = 0; i < this.nodes.length; i ++){
-		if(arr[i] != undefined){
-			this.nodes[i].val = arr[i] || 0;
+	function Layer(nodes){
+		this.nodes = [];
+		for(var i = 0; i < nodes; i ++){
+			this.nodes[i] = new Node(Math.random()*2-1, i, nodes);
 		}
-	}
-};
-Layer.prototype.setInput = function(layer){
-	this.inputLayer = layer;
-	for(var i = 0; i < this.nodes.length; i ++){
-		for(var j = 0; j < layer.nodes.length; j ++){
-			this.nodes[i].addInput(layer.nodes[j]);
-		}
-	}
-};
-Layer.prototype.update = function(){
-	for(var i = 0; i < this.nodes.length; i ++){
-		this.nodes[i].update();
-	}
-};
-Layer.prototype.display = function(x,px){
-	var nh = 20;
-	for(var i = 0; i < this.nodes.length; i ++){
-		for(var j = 0; j < this.nodes[i].inputs.length; j ++){
-			var w = this.nodes[i].weights[j];
-
-			strokeWeight(abs(w)*nh/50);
-			if(w>0){
-				stroke(0, 255, 0);
-			} else {
-				stroke(255, 0, 0);
+		this.inputLayer = [];
+	};
+	Layer.prototype.set = function(arr){
+		for(var i = 0; i < this.nodes.length; i ++){
+			if(arr[i] != undefined){
+				this.nodes[i].val = arr[i] || 0;
 			}
-			var n = this.nodes[i].inputs[j];
-			var mn = this.nodes[i];
-			line(px,map(n.index,0,n.outOf,-n.outOf*nh,n.outOf*nh)+height/2,x,map(mn.index,0,mn.outOf,-mn.outOf*nh,mn.outOf*nh)+height/2);
 		}
-	}
-	strokeWeight(nh/20);
-  stroke(0);
-	for(var i = 0; i < this.nodes.length; i ++){
-		fill(this.val*255);
-		var mn = this.nodes[i];
-		var y = map(mn.index,0,mn.outOf,-mn.outOf*nh,mn.outOf*nh)+height/2;
-		this.nodes[i].display(x,y,nh);
-	}
-};
-Layer.prototype.get = function(){
-	var out = [];
-	for(var i = 0; i < this.nodes.length; i ++){
-		out.push(this.nodes[i].val);
-	}
-	return out;
-};
-Layer.prototype.evolve = function(learn){
-	for(var i = 0; i < this.nodes.length; i ++){
-		this.nodes[i].randomize(learn);
-	}
-};
-Layer.prototype.copy = function(){
+	};
+	Layer.prototype.setInput = function(layer){
+		this.inputLayer = layer;
+		for(var i = 0; i < this.nodes.length; i ++){
+			for(var j = 0; j < layer.nodes.length; j ++){
+				this.nodes[i].addInput(layer.nodes[j]);
+			}
+		}
+	};
+	Layer.prototype.update = function(){
+		for(var i = 0; i < this.nodes.length; i ++){
+			this.nodes[i].update();
+		}
+	};
+	Layer.prototype.display = function(x,my,px,h){
+		var nh = h;
+		for(var i = 0; i < this.nodes.length; i ++){
+			for(var j = 0; j < this.nodes[i].inputs.length; j ++){
+				var w = this.nodes[i].weights[j];
 
-};
+				strokeWeight(abs(w)*nh/50);
+				if(w>0){
+					stroke(0, 255, 0);
+				} else {
+					stroke(255, 0, 0);
+				}
+				var n = this.nodes[i].inputs[j];
+				var mn = this.nodes[i];
+				line(px,map(n.index,0,n.outOf,-n.outOf*nh,n.outOf*nh)+my,x,map(mn.index,0,mn.outOf,-mn.outOf*nh,mn.outOf*nh)+my);
+			}
+		}
+		strokeWeight(nh/20);
+		stroke(0);
+		for(var i = 0; i < this.nodes.length; i ++){
+			fill(this.val*255);
+			var mn = this.nodes[i];
+			var y = map(mn.index,0,mn.outOf,-mn.outOf*nh,mn.outOf*nh)+my;
+			this.nodes[i].display(x,y,nh);
+		}
+	};
+	Layer.prototype.get = function(){
+		var out = [];
+		for(var i = 0; i < this.nodes.length; i ++){
+			out.push(this.nodes[i].val);
+		}
+		return out;
+	};
+	Layer.prototype.evolve = function(learn){
+		for(var i = 0; i < this.nodes.length; i ++){
+			this.nodes[i].randomize(learn);
+		}
+	};
+	Layer.prototype.copy = function(){
+		var newLay = new Layer(0);
+		for(var i = 0; i < this.nodes.length; i ++){
+			newLay.nodes[i] = this.nodes[i].copy();
+		}
+		return newLay;
+	};
 
 }
 
@@ -152,52 +152,66 @@ Layer.prototype.copy = function(){
 
 {
 
-function Network(layers) {
-	if(layers.length < 2) {
-		return alert("Not enough layers for a Network");
-	}
-	this.inputLayer = new Layer(layers[0]);
-	this.outputlayer = new Layer(layers[layers.length-1]);
-	if(layers.length > 2) {
-		this.hiddenLayers = [];
-		for(var i = 1; i < layers.length-1; i ++){
-			this.hiddenLayers[i-1] = new Layer(layers[i]);
-			if(i == 1){
-				this.hiddenLayers[i-1].setInput(this.inputLayer)
-			} else {
-				this.hiddenLayers[i-1].setInput(this.hiddenLayers[i-2]);
-			}
+	function Network(layers) {
+		if(layers.length < 2) {
+			return alert("Not enough layers for a Network");
 		}
-	} else {
-		this.outputLayer.setInput(this.inputLayer);
-	}
-};
-Network.prototype.setInput = function(data){
-	this.inputLayer.set(data);
-};
-Network.prototype.getOutput = function() {
-	return this.outputLayer.get();
-};
-Network.prototype.update = function(){
-	for(var i = 0; i < this.hiddenLayers.length; i ++){
-		this.hiddenLayers[i].update();
-	}
-	this.outputLayer.update();
-};
-Netowrk.prototype.evolve = function(learn){
-	this.outputLayer.evolve(learn);
-	for(var i = 0; i < this.hiddenLayers.length; i ++){
-		this.hiddenLayers[i].evolve(learn);
-	}
-};
-Network.prototype.copy = function(){
-	var newNet = {};
-	newNet.inputLayer = this.inputLayer.copy();
-	newNet.outputLayer = this.outputLayer.copy();
-	newNet.hiddenLayers = [];
-	for(var i = 0; i < this.hiddenLayers.length; i ++){
-		newNet.hiddenLayers[i] = this.hiddenLayers[i];
-	}
-};
+		this.inputLayer = new Layer(layers[0]);
+		this.outputlayer = new Layer(layers[layers.length-1]);
+		this.hiddenLayers = [];
+		if(layers.length > 2) {
+			for(var i = 1; i < layers.length-1; i ++){
+				this.hiddenLayers[i-1] = new Layer(layers[i]);
+				if(i == 1){
+					this.hiddenLayers[i-1].setInput(this.inputLayer)
+				} else {
+					this.hiddenLayers[i-1].setInput(this.hiddenLayers[i-2]);
+				}
+			}
+		} else {
+			this.outputLayer.setInput(this.inputLayer);
+		}
+	};
+	Network.prototype.setInput = function(data){
+		this.inputLayer.set(data);
+	};
+	Network.prototype.getOutput = function() {
+		return this.outputLayer.get();
+	};
+	Network.prototype.update = function(){
+		for(var i = 0; i < this.hiddenLayers.length; i ++){
+			this.hiddenLayers[i].update();
+		}
+		this.outputLayer.update();
+	};
+	Network.prototype.display = function(x,my,w,nh) {
+		var layers = 2 + this.hiddenLayers.length
+		this.outputLayer.display()
+	};
+	Network.prototype.evolve = function(learn){
+		this.outputLayer.evolve(learn);
+		for(var i = 0; i < this.hiddenLayers.length; i ++){
+			this.hiddenLayers[i].evolve(learn);
+		}
+	};
+	Network.prototype.copy = function(){
+		var newNet = {};
+		newNet.inputLayer = this.inputLayer.copy();
+		newNet.outputLayer = this.outputLayer.copy();
+		if(this.hiddenLayers.length>0){
+			for(var i = 0; i < this.hiddenLayers.length; i ++){
+				newNet.hiddenLayers[i] = this.hiddenLayers[i].copy();
+				if(i == 0) {
+					newNet.hiddenLayers[i].setInput(newNet.inputLayer);
+				} else {
+					newNet.hiddenLayers[i].setInput(newNet.hiddenLayers[i-1]);
+				}
+			}
+			newNet.outputLayer.setInput(newNet.hiddenLayers[newNet.hiddenLayers.length-1]);
+		} else {
+			newNet.outputLayer.setInput(newNet.inputLayer);
+		}
+		return newNet;
+	};
 
 }
